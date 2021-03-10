08b5623bffd5a190fc118063e7caaf8c92318293
Griffin Prechter <griffinprechter@MacBook-Pro-2.local>
2021-03-09 20:17:58 -0800

    Some changes to core operators to emit SMTLIB

---
+++ src/DSEState/DSEState.c
@@ -0,0 +1,5 @@
+//
+// Created by Griffin Prechter on 3/7/21.
+//
+
+#include "DSEState.h"
---
+++ src/DSEState/DSEState.h
@@ -0,0 +1,8 @@
+//
+// Created by Griffin Prechter on 3/7/21.
+//
+
+#ifndef YOUVERIFY_DSESTATE_H
+#define YOUVERIFY_DSESTATE_H
+
+#endif //YOUVERIFY_DSESTATE_H
